8e2501cc82774f8d1ffed661ccc4aa2b20db1a34
Your Name <you@example.com>
2023-01-13 06:39:48 -0500

    inital backend server commit

---
+++ server.js
@@ -0,0 +1,27 @@
+//--------------------------------------
+//  Dependencies
+//--------------------------------------
+
+//get .env variables
+const{ PORT = 3000 } = process.env;
+
+//import express
+const express = require("express");
+
+//create application object
+const app=express();
+
+//--------------------------------------
+//  Routes
+//--------------------------------------
+
+//create a test route
+app.get("/", (req, res) => {
+    res.send("hello world");
+});
+
+//--------------------------------------
+//  Listener
+//--------------------------------------
+
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`))
